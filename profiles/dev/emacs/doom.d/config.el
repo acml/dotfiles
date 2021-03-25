141a793739aff01995c719291e2ea0da28b96e13
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2021-03-26 01:07:47 +0300

    new doom emacs configuration

--- profiles/dev/emacs/doom.d/config.el
+++ profiles/dev/emacs/doom.d/config.el
@@ -1,49 +1,91 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el --- My personal configuration -*- lexical-binding: t; -*-
+;;; Commentary:
 
+;;; Code:
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
-
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Ahmet Cemal Özgezer"
-      user-mail-address "ahmet.ozgezer@andasis.com")
+      user-mail-address "ozgezer@gmail.com"
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (when (equal system-type 'gnu/linux)
-;;   (setq doom-font (font-spec :family "Iosevka" :size 14)
-;;         doom-variable-pitch-font (font-spec :family "Ubuntu Nerd Font" :size 14)))
-;; (when (equal system-type 'darwin)
-;;   (setq doom-font (font-spec :family "Menlo" :size 14)
-;;         doom-variable-pitch-font (font-spec :family "Menlo" :size 14)))
-;; (when (equal system-type 'windows-nt)
-;;   (setq doom-font (font-spec :family "InputMono" :size 18)
-;;         doom-big-font (font-spec :family "InputMono" :size 22)))
-(setq doom-font (font-spec :family "Iosevka Nerd Font" :size 13)
-      doom-variable-pitch-font (font-spec :family "Iosevka Nerd Font" :size 13))
+      ;; There are two ways to load a theme. Both assume the theme is installed and
+      ;; available. You can either set `doom-theme' or manually load a theme with the
+      ;; `load-theme' function. This is the default:
+      doom-theme 'modus-operandi
+      ;; doom-theme 'doom-one
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'modus-operandi)
+      ;; This determines the style of line numbers in effect. If set to `nil', line
+      ;; numbers are disabled. For relative line numbers, set this to `relative'.
+      ;;
+      ;; Line numbers are pretty slow all around. The performance boost of
+      ;; disabling them outweighs the utility of always keeping them on.
+      display-line-numbers-type nil
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Documents/org/")
-(setq org-noter-notes-search-path '("~/Documents/org/notes/"))
+      ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+      ;; are the three important ones:
+      ;;
+      ;; + `doom-font'
+      ;; + `doom-variable-pitch-font'
+      ;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+      ;;   presentations or streaming.
+      ;;
+      ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+      ;; font string. You generally only need these two:
+      ;; (setq doom-font (font-spec :family "Iosevka" :size 14)
+      ;;       doom-variable-pitch-font (font-spec :family "Ubuntu Nerd Font" :size 14))
+      doom-font (font-spec :family "Iosevka Nerd Font" :size 13)
+      doom-big-font (font-spec :family "Iosevka Nerd Font" :size 26)
+      doom-variable-pitch-font (font-spec :family "Overpass Nerd Font" :size 13)
+      doom-serif-font (font-spec :family "BlexMono Nerd Font" :weight 'light)
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+      fancy-splash-image (concat doom-private-dir "splash.png")
+
+      auth-sources '("~/.authinfo.gpg")
+      auth-source-cache-expiry nil ; default is 7200 (2h)
+
+      calendar-location-name "Istanbul, Turkey"
+      calendar-latitude 41.168602
+      calendar-longitude 29.047024
+
+      delete-by-moving-to-trash t  ; Delete files to trash
+      window-combination-resize t  ; take new window space from all other windows (not just current)
+      x-stretch-cursor t           ; Stretch cursor to the glyph width
+      undo-limit 80000000          ; Raise undo-limit to 80Mb
+      evil-want-fine-undo t        ; By default while in insert all changes are one big blob. Be more granular
+      auto-save-default t          ; Nobody likes to loose work, I certainly don't
+      truncate-string-ellipsis "…" ; Unicode ellispis are nicer than "...", and also save /precious/ space
+
+      ;; Switch to the new window after splitting
+      evil-vsplit-window-right t
+      evil-split-window-below t
+
+      +ivy-buffer-preview t
+
+      frame-title-format
+      '(""
+        (:eval
+         (if (s-contains-p org-roam-directory (or buffer-file-name ""))
+             (replace-regexp-in-string
+              ".*/[0-9]*-?" "☰ "
+              (subst-char-in-string ?_ ?  buffer-file-name))
+           "%b"))
+        (:eval
+         (let ((project-name (projectile-project-name)))
+           (unless (string= "-" project-name)
+             (format (if (buffer-modified-p)  " ◉ %s" "  ●  %s") project-name))))))
+
+;; (if (equal "Battery status not available"
+;;            (battery))
+;;     (display-battery-mode 1)                        ; On laptops it's nice to know how much power you have
+;;   (setq password-cache-expiry nil))               ; I can trust my desktops ... can't I? (no battery = desktop)
+
+(global-subword-mode 1)                           ; Iterate through CamelCase words
+
+(setq-default custom-file (expand-file-name ".custom.el" doom-private-dir))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
@@ -62,9 +104,17 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(when (window-system)
-  (add-to-list 'default-frame-alist '(alpha . (100)))
-  (set-frame-parameter (selected-frame) 'alpha '(100)))
+;; Prevents some cases of Emacs flickering
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
+
+(add-to-list 'default-frame-alist '(alpha . (95)))
+(set-frame-parameter (selected-frame) 'alpha '(95))
+
+(when (daemonp)
+  (add-hook 'after-make-frame-functions
+            (lambda (frame)
+              (with-selected-frame frame
+                (modus-themes-load-operandi)))))
 
 (set-popup-rules! '(("^\\*Customize.*" :slot 2 :side right :modeline nil :select t :quit t)
                     (" \\*undo-tree\\*" :slot 2 :side left :size 20 :modeline nil :select t :quit t)
@@ -89,11 +139,79 @@
 
                     ;; * lang
                     ;; ** python
-                    ("^\\*Anaconda\\*" :side right :size 82 :quit t :ttl t)
-                    ))
+                    ("^\\*Anaconda\\*" :side right :size 82 :quit t :ttl t)))
 
 (after! ivy-posframe
   (setq ivy-posframe-border-width 3))
+
+(after! persp-mode
+  (setq persp-emacsclient-init-frame-behaviour-override nil)
+  (defun display-workspaces-in-minibuffer ()
+    (with-current-buffer " *Minibuf-0*"
+      (erase-buffer)
+      (insert (+workspace--tabline))))
+  (run-with-idle-timer 1 t #'display-workspaces-in-minibuffer)
+  (+workspace/display))
+
+;; (defconst ac/c-or-c++-mode--regexp
+;;   (eval-when-compile
+;;     (let ((id "[a-zA-Z0-9_]+") (ws "[ \t\r]+") (ws-maybe "[ \t\r]*"))
+;;       (concat "^" ws-maybe "\\(?:"
+;;                     "using"     ws "\\(?:namespace" ws "std;\\|std::\\)"
+;;               "\\|" "namespace" "\\(:?" ws id "\\)?" ws-maybe "{"
+;;               "\\|" "class"     ws id ws-maybe "[:{\n]"
+;;               "\\|" "template"  ws-maybe "<.*>"
+;;               "\\|" "#include"  ws-maybe "<\\(?:string\\|iostream\\|map\\)>"
+;;               "\\)")))
+;;   "A regexp applied to C header files to check if they are really C++.")
+
+;; (defconst ac/make-mode--regexp
+;;   (eval-when-compile
+;;     (let ((id "[A-Z0-9_]+") (ws-maybe "[ \t\r]*"))
+;;       (concat "^" ws-maybe "\\(?:"
+;;               "include"  ws-maybe ".*"
+;;               "\\|" id ws-maybe "=" ws-maybe ".*"
+;;               "\\)")))
+;;   "A regexp applied to files to check if they are really Makefiles.")
+
+;; (defun ac/c-or-make-mode ()
+;;   "Analyze buffer and enable either C or C++ mode.
+;; Some people and projects use .h extension for C++ header files
+;; which is also the one used for C header files.  This makes
+;; matching on file name insufficient for detecting major mode that
+;; should be used.
+;; This function attempts to use file contents to determine whether
+;; the code is C or C++ and based on that chooses whether to enable
+;; `c-mode' or `c++-mode'."
+;;   (save-excursion
+;;     (save-restriction
+;;       (save-match-data
+;;         (widen)
+;;         (goto-char (point-min))
+;;         (if (re-search-forward ac/c-or-c++-mode--regexp
+;;                                (+ (point) 50000) t)
+;;             (c++-mode)
+;;           (goto-char (point-min))
+;;           (if (re-search-forward ac/make-mode--regexp
+;;                                  (+ (point) 50000) t)
+;;               (makefile-mode)
+;;             (c-mode)))))))
+
+;; (add-to-list 'auto-mode-alist '("\\.h\\'" . ac/c-or-make-mode))
+
+;; (defun ac/make-mode-p ()
+;;   "Analyze buffer and enable Makefile mode.
+;; This function attempts to use file contents to determine whether
+;; the code is Makefile and based on that chooses whether to enable
+;; `makefile-mode'."
+;;   (save-excursion
+;;     (save-restriction
+;;       (save-match-data
+;;         (widen)
+;;         (goto-char (point-min))
+;;         (re-search-forward ac/make-mode--regexp
+;;                                  (+ (point) magic-mode-regexp-match-limit) t)))))
+;; (add-to-list 'magic-mode-alist '(ac/make-mode-p . makefile-mode))
 
 (setq-default
  delete-by-moving-to-trash t)
@@ -144,13 +262,6 @@
   (setq ccls-initialization-options `(:index (:comments 2)
                                       :completion (:detailedLabel t)
                                       :cache (:directory ,(file-truename "~/.cache/ccls")))))
-
-(setq calendar-location-name "Istanbul, Turkey"
-      calendar-latitude 41.168602
-      calendar-longitude 29.047024)
-
-;;; :ui doom-dashboard
-(setq fancy-splash-image (concat doom-private-dir "splash.png"))
 
 (map! "M-c" #'capitalize-dwim
       "M-l" #'downcase-dwim
@@ -217,13 +328,26 @@
 (use-package! docker-tramp)
 (use-package! docker)
 
-(use-package! atomic-chrome
-  :after-call focus-out-hook
-  :config
-  (setq atomic-chrome-default-major-mode 'markdown-mode
-        atomic-chrome-buffer-open-style 'frame)
-  (atomic-chrome-start-server))
+;; Easier to match with a bspwm rule:
+;;   bspc rule -a 'Emacs:emacs-everywhere' state=floating sticky=on
+(setq emacs-everywhere-frame-name-format "emacs-everywhere")
 
+;; The modeline is not useful to me in the popup window. It looks much nicer
+;; to hide it.
+(add-hook 'emacs-everywhere-init-hooks #'hide-mode-line-mode)
+
+;; Semi-center it over the target window, rather than at the cursor position
+;; (which could be anywhere).
+(defadvice! my-emacs-everywhere-set-frame-position (&rest _)
+  :override #'emacs-everywhere-set-frame-position
+  (cl-destructuring-bind (width . height)
+      (alist-get 'outer-size (frame-geometry))
+    (set-frame-position (selected-frame)
+                        (+ emacs-everywhere-window-x
+                           (/ emacs-everywhere-window-width 2)
+                           (- (/ width 2)))
+                        (+ emacs-everywhere-window-y
+                           (/ emacs-everywhere-window-height 2)))))
 (use-package! highlight-parentheses
   :init
   (add-hook 'prog-mode-hook #'highlight-parentheses-mode)
@@ -309,82 +433,74 @@
   ;; Set customization options to values of your choice
   (setq modus-themes-slanted-constructs t
         modus-themes-bold-constructs t
-        modus-themes-fringes nil ; {nil,'subtle,'intense}
-
-        ;; Options for `modus-themes-lang-checkers': nil,
-        ;; 'straight-underline, 'subtle-foreground,
-        ;; 'subtle-foreground-straight-underline, 'intense-foreground,
-        ;; 'intense-foreground-straight-underline, 'colored-background
-        modus-themes-lang-checkers 'straight-underline
-
-        ;; Options for `modus-themes-mode-line': nil, '3d, 'moody,
-        ;; 'borderless, 'borderless-3d, 'borderless-moody
-        modus-themes-mode-line 'borderless-3d
-
-        ;; Options for `modus-themes-syntax': nil, 'faint,
-        ;; 'yellow-comments, 'green-strings,
-        ;; 'yellow-comments-green-strings, 'alt-syntax,
-        ;; 'alt-syntax-yellow-comments
-        modus-themes-syntax 'faint
-
-        modus-themes-intense-hl-line nil
-        modus-themes-subtle-line-numbers nil
-        modus-themes-paren-match 'subtle-bold ; {nil,'subtle-bold,'intense,'intense-bold}
-
-        ;; Options for `modus-themes-links': nil, 'faint,
-        ;; 'neutral-underline, 'faint-neutral-underline, 'no-underline,
-        ;; 'underline-only
-        modus-themes-links 'no-underline
-
+        modus-themes-variable-pitch-headings t
+        modus-themes-variable-pitch-ui t
         modus-themes-no-mixed-fonts nil
-
-        ;; Options for `modus-themes-prompts': nil, 'subtle-accented,
-        ;; 'intense-accented, 'subtle-gray, 'intense-gray
-        modus-themes-prompts 'subtle-accented
-
-        modus-themes-completions 'opinionated ; {nil,'moderate,'opinionated}
-        modus-themes-region 'bg-only-no-extend ; {nil,'no-extend,'bg-only,'bg-only-no-extend}
-        modus-themes-diffs 'desaturated ; {nil,'desaturated,'fg-only,'bg-only}
-        modus-themes-org-blocks nil ; {nil,'grayscale,'rainbow}
-        modus-themes-org-habit nil ; {nil,'simplified,'traffic-light}
-
-        modus-themes-headings ; Lots of options---continue reading the manual
+        modus-themes-headings
         '((1 . section)
           (2 . section-no-bold)
           (3 . rainbow-line)
           (t . rainbow-line-no-bold))
-
-        modus-themes-variable-pitch-ui t
-        modus-themes-variable-pitch-headings t
         modus-themes-scale-headings t
-        modus-themes-scale-1 1.1
-        modus-themes-scale-2 1.15
-        modus-themes-scale-3 1.21
-        modus-themes-scale-4 1.27
-        modus-themes-scale-5 1.33)
+        modus-themes-fringes nil
+        modus-themes-lang-checkers nil
+        modus-themes-org-blocks nil
+        modus-themes-org-habit nil
+        modus-themes-prompts 'subtle-accented
+        modus-themes-mode-line 'borderless-3d
+        modus-themes-diffs 'desaturated
+        modus-themes-syntax 'faint
+        modus-themes-hl-line 'accented-background
+        modus-themes-subtle-line-numbers t
+        modus-themes-paren-match 'subtle-bold
+        modus-themes-region 'bg-only-no-extend
+        modus-themes-links 'faint-neutral-underline
+        modus-themes-completions 'opinionated
+
+        modus-themes-scale-1 1.05
+        modus-themes-scale-2 1.1
+        modus-themes-scale-3 1.15
+        modus-themes-scale-4 1.2
+        modus-themes-scale-5 1.3)
 
   ;; Load the theme files before enabling a theme
-  ;; (modus-themes-load-themes)
+  (modus-themes-load-themes)
 
   :config
   ;; Load the theme of your choice
   ;; (modus-themes-load-operandi)
   ;; ;; OR
   ;; (load-theme 'modus-operandi t)
-  :bind ("<f5>" . modus-themes-toggle))
+  :bind ("<f5>" . modus-themes-toggle)
+  )
+
+(use-package! org
+  :init
+  (setq
+      ;; If you use `org' and don't want your org files in the default location below,
+      ;; change `org-directory'. It must be set before org loads!
+      org-directory "~/Documents/org/"
+      ;; ;; org-noter-notes-search-path '("~/Documents/org/notes/")
+      org-archive-location (concat org-directory ".archive/%s::")
+      org-roam-directory (concat org-directory "notes/")
+      org-roam-db-location (concat org-roam-directory ".org-roam.db")
+      org-journal-encrypt-journal t
+      org-journal-file-format "%Y%m%d.org"
+      org-startup-folded 'overview
+      org-ellipsis " [...] "))
 
 (use-package! rainbow-mode
   :hook
   ((prog-mode . rainbow-mode)
    (org-mode . rainbow-mode)))
 
-;; (use-package! shrface)
-
 (use-package! trashed
   :config
   (add-to-list 'evil-emacs-state-modes 'trashed-mode))
 
 (use-package! treemacs
+  :init
+  (defvar treemacs-no-load-time-warnings t)
   :config
   (setq treemacs-collapse-dirs (if (executable-find "python") 3 0)
         treemacs-eldoc-display t
--- profiles/dev/emacs/doom.d/init.el
+++ profiles/dev/emacs/doom.d/init.el
@@ -39,7 +39,7 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        ;;indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
@@ -91,7 +91,7 @@
        ;;ansible
        (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
        direnv
-       docker
+       (docker +lsp)
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
@@ -138,7 +138,7 @@
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
@@ -151,7 +151,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +brain +dragndrop +gnuplot +journal +noter +pandoc +pretty +roam)               ; organize your plain life in plain text
        php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -165,7 +165,7 @@
        (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
-       (sh +lsp)                ; she sells {ba,z,fi}sh shells on the C xor
+       (sh +lsp +powershell)                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
@@ -179,7 +179,9 @@
        ;;(wanderlust +gmail)
 
        :app
-       calendar
+       ;;calendar
+       ;; emms
+       everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
